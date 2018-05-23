888cf96405ab2afffba51609691737ecf1839e4b
Kleberson Santos <kleberson.santos@itbam.org.br>
2018-05-23 17:45:52 -0400

    authentication in memory with JWT

--- recursos-api/src/main/java/org/itbam/recourses/resources/Recourse.java
+++ recursos-api/src/main/java/org/itbam/recourses/resources/Recourse.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.itbam.recourses.repository.Recourses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ public class Recourse {
 	
 	@Autowired
 	private Recourses recourses;
+	
+	@GetMapping("/")
+	public String home() {
+		return "Home Page";
+	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<org.itbam.recourses.model.Recourse> all(){
